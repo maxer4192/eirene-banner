b8a1abe7a923d254ed0320f2e603dfeb565f5805
Maddox Albro <74218483+maxer4192@users.noreply.github.com>
2022-05-06 15:40:17 -0400

    Update App.js
    Change the authentication type

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,27 @@ import React from "react";
 import "../styles/App.scss";
 import Editor from "./Editor";
 import SuperTokens from "supertokens-auth-react";
-import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
+import ThirdParty, {Github, Google, Facebook, Apple} from "supertokens-auth-react/recipe/thirdparty";
 import Session from "supertokens-auth-react/recipe/session";
 
 SuperTokens.init({
-  appInfo: {
-      // learn more about this on https://supertokens.com/docs/emailpassword/appinfo
-      appName: "Eirene Network",
-      apiDomain: "http://localhost:3567",
-      websiteDomain: "https://banner.eirene-network.xyz",
-      apiBasePath: "/auth",
-      websiteBasePath: "/auth",
-  },
-  recipeList: [
-      EmailPassword.init(),
-      Session.init()
-  ]
+    appInfo: {
+        appName: "Eirene Network",
+        apiDomain: "http://localhost:8080",
+        websiteDomain: "https://banner.eirene-network.xyz",
+        apiBasePath: "/auth",
+        websiteBasePath: "/auth"
+    },
+    recipeList: [
+        ThirdParty.init({
+            signInAndUpFeature: {
+                providers: [
+                    Google.init()
+                ]
+            }
+        }),
+        Session.init()
+    ]
 });
 class App extends React.Component {
   render() {
